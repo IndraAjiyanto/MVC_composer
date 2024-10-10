d466a282ac3647867a3821ab15284874400fad39
IndraAjiyanto <indraajiyanto052@gmail.com>
2024-10-10 14:54:18 +0700

    controller

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Indra\Mvc\Controller;
+
+class HomeController{
+    public function hello(){
+        echo "HELOO";
+    }
+    public function world(){
+        echo "WORLD";
+    }
+    public function index(){
+        echo "index";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,9 +3,10 @@
 require_once "../vendor/autoload.php";
 
 use Indra\Mvc\App\Router;
+use Indra\Mvc\Controller\HomeController;
 
-Router::add('GET', '/index.php', "HomeController", "index");
-Router::add('GET', '/login', "HomeController", "login");
-Router::add('GET', '/register', "HomeController", "register");
+Router::add('GET', '/index.php', HomeController::class , "index");
+Router::add('GET', '/hello', HomeController::class, "hello");
+Router::add('GET', '/world', HomeController::class, "world");
 
 Router::run();
